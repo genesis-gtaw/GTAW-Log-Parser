71572f18bbf12a62d61ad664a96fd45533b942ce
David <david.cristian.horvath@gmail.com>
2018-09-22 21:38:21 +0300

    Remove threading

--- Parser/Main.cs
+++ Parser/Main.cs
@@ -2,16 +2,18 @@
 using System.Windows.Forms;
 
 using System.IO;
-using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Threading;
 using System.Text.RegularExpressions;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
 using HtmlAgilityPack;
 
 namespace Parser
 {
     public partial class Main : Form
     {
-        private static readonly string path = Data.logLogation;
+        private Thread parseThread;
+        private Thread updateThread;
 
         public Main()
         {
@@ -22,7 +24,7 @@ namespace Parser
 
         private void SaveSettings()
         {
-            Properties.Settings.Default.Path = Path.Text;
+            Properties.Settings.Default.Path = FolderPath.Text;
             Properties.Settings.Default.CheckForUpdates = CheckForUpdatesOnStartup.Checked;
 
             Properties.Settings.Default.Save();
@@ -30,92 +32,78 @@ namespace Parser
 
         private void LoadSettings()
         {
-            Path.Text = Properties.Settings.Default.Path;
+            FolderPath.Text = Properties.Settings.Default.Path;
             CheckForUpdatesOnStartup.Checked = Properties.Settings.Default.CheckForUpdates;
 
             Version.Text = $"Version: {Properties.Settings.Default.Version}";
+        }
+
+        private void FolderPath_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (FolderPath.Text.Length == 0)
+            {
+                Browse_Click(this, EventArgs.Empty);
+            }
         }
 
         private void Browse_Click(object sender, EventArgs e)
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog
             {
-                InitialDirectory = System.IO.Path.GetPathRoot(Environment.SystemDirectory),
+                InitialDirectory = Path.GetPathRoot(Environment.SystemDirectory),
                 IsFolderPicker = true
             };
 
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                Path.Text = dialog.FileName;
-                Path.Focus();
-                Path.SelectionStart = Path.Text.Length;
+                FolderPath.Text = dialog.FileName;
+                FolderPath.Focus();
+                FolderPath.SelectionStart = FolderPath.Text.Length;
                 SaveSettings();
-            }
-        }
-
-        private void Path_MouseClick(object sender, MouseEventArgs e)
-        {
-            if (Path.Text.Length == 0)
-            {
-                Browse_Click(this, EventArgs.Empty);
-            }
-        }
-
-        private void TryToParseLog(string folder)
-        {
-            new Thread(() =>
-            {
-                try
-                {
-                    StreamReader sr = new StreamReader(folder + path);
-                    string log = sr.ReadToEnd();
-                    sr.Close();
-
-                    log = log.Remove(0, 1);
-
-                    log = Regex.Replace(log, "{endl}", Environment.NewLine);
-                    log = Regex.Replace(log, "~[a-zA-Z]~", "");
-
-                    log = log.Remove(log.Length - 3, 3);
-
-                    SetText(log);
-                }
-                catch
-                {
-                    MessageBox.Show("An error occured while parsing the chat log.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-            }).Start();
-        }
-
-        delegate void SetTextCallback(string text);
-        private void SetText(string text)
-        {
-            if (Parsed.InvokeRequired)
-            {
-                SetTextCallback d = new SetTextCallback(SetText);
-                Invoke(d, new object[] { text });
-            }
-            else
-            {
-                Parsed.Text = text;
             }
         }
 
         private void Parse_Click(object sender, EventArgs e)
         {
-            if (Path.Text.Length == 0)
+            if (FolderPath.Text.Length == 0)
             {
                 MessageBox.Show("Invalid RAGEMP folder path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            else if (!File.Exists(Path.Text + path))
+            else if (!File.Exists(FolderPath.Text + Data.logLogation))
             {
                 MessageBox.Show("Can't find the GTA World chat log.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            TryToParseLog(Path.Text);
+            TryToParseLog(FolderPath.Text);
+        }
+
+        private void TryToParseLog(string folderPath)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(folderPath + Data.logLogation);
+                string log = sr.ReadToEnd();
+                sr.Close();
+
+                log = log.Remove(0, 1);
+
+                log = Regex.Replace(log, "{endl}", "\n");
+                log = Regex.Replace(log, "~[a-zA-Z]~", "");
+
+                log = log.Remove(log.Length - 3, 3);
+
+                Parsed.Text = log;
+
+                int numLines = log.Split('\n').Length;
+
+                MessageBox.Show($"{numLines}", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("An error occured while parsing the chat log.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void CheckForUpdatesOnStartup_CheckedChanged(object sender, EventArgs e)
@@ -128,65 +116,55 @@ namespace Parser
 
         private void CheckForUpdates(bool checking = false)
         {
-            new Thread(() =>
+            try
             {
-                try
+                float installedVersion = Properties.Settings.Default.Version;
+                string unparsedWebVersion = "";
+
+                string url = Data.versionLocation;
+                HtmlWeb web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+
+                var metaTags = doc.DocumentNode.SelectNodes("//meta");
+                foreach (var tag in metaTags)
                 {
-                    float installedVersion = Properties.Settings.Default.Version;
+                    var tagName = tag.Attributes["name"];
+                    var tagContent = tag.Attributes["content"];
+                    var tagProperty = tag.Attributes["property"];
 
-                    string unparsedWebVersion = "";
-
-                    string url = Data.versionLocation;
-                    HtmlWeb web = new HtmlWeb();
-                    HtmlAgilityPack.HtmlDocument doc = web.Load(url);
-
-                    var metaTags = doc.DocumentNode.SelectNodes("//meta");
-                    foreach (var tag in metaTags)
+                    if (tagProperty != null && tagContent != null)
                     {
-                        var tagName = tag.Attributes["name"];
-                        var tagContent = tag.Attributes["content"];
-                        var tagProperty = tag.Attributes["property"];
-                        if (tagProperty != null && tagContent != null)
+                        switch (tagProperty.Value.ToLower())
                         {
-                            switch (tagProperty.Value.ToLower())
-                            {
-                                case "og:description":
-                                    unparsedWebVersion = tagContent.Value;
-                                    break;
-                            }
+                            case "og:description":
+                                unparsedWebVersion = tagContent.Value;
+                                break;
                         }
                     }
-
-                    var match = Regex.Match(unparsedWebVersion, @"([-+]?[0-9]*\.?[0-9]+)");
-                    float currentVersion = Convert.ToSingle(match.Groups[1].Value);
-
-                    if (currentVersion > installedVersion)
-                    {
-                        if (MessageBox.Show($"A new version of the chat log parser is now available on GitHub.\n\nInstalled Version: {installedVersion}\nAvailable Version: {currentVersion}\n\nWould you like to visit the releases page now?", "Update Available", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
-                            System.Diagnostics.Process.Start("https://github.com/MapleToo/GTAW-Log-Parser/releases");
-                    }
-                    else if (currentVersion < installedVersion && checking)
-                    {
-                        if (MessageBox.Show($"You are using a newer version of the chat log parser than is recommended. You may encounter unwated errors, continue at your own discretion.\n\nInstalled Version: {installedVersion}\nRecommended Version: {currentVersion}\n\nWould you like to visit the releases page now?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
-                            System.Diagnostics.Process.Start("https://github.com/MapleToo/GTAW-Log-Parser/releases");
-                    }
-                    else if (checking)
-                        MessageBox.Show($"You are running the latest version of the chat log parser.\n\nInstalled Version: {installedVersion}", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
                 }
-                catch
+
+                var match = Regex.Match(unparsedWebVersion, @"([-+]?[0-9]*\.?[0-9]+)");
+                float currentVersion = Convert.ToSingle(match.Groups[1].Value);
+
+                if (currentVersion > installedVersion)
                 {
-                    if (checking)
-                        MessageBox.Show($"No updates could be found, try checking your internet connection.\n\nInstalled Version: {Properties.Settings.Default.Version}", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (MessageBox.Show($"A new version of the chat log parser is now available on GitHub.\n\nInstalled Version: {installedVersion}\nAvailable Version: {currentVersion}\n\nWould you like to visit the releases page now?", "Update Available", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                        System.Diagnostics.Process.Start("https://github.com/MapleToo/GTAW-Log-Parser/releases");
                 }
+                //else if (currentVersion < installedVersion && checking)
+                //{
+                //    if (MessageBox.Show($"You are using a newer version of the chat log parser than is recommended. You may encounter unwated errors, continue at your own discretion.\n\nInstalled Version: {installedVersion}\nRecommended Version: {currentVersion}\n\nWould you like to visit the releases page now?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                //        System.Diagnostics.Process.Start("https://github.com/MapleToo/GTAW-Log-Parser/releases");
+                //}
+                else if (checking)
+                    MessageBox.Show($"You are running the latest version of the chat log parser.\n\nInstalled Version: {installedVersion}", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            }).Start();
-        }
-
-        private void CopyParsedToClipboard_Click(object sender, EventArgs e)
-        {
-            if (Parsed.Text.Length != 0)
-                Clipboard.SetText(Parsed.Text.Replace("\n", Environment.NewLine));
+            }
+            catch
+            {
+                if (checking)
+                    MessageBox.Show($"No updates could be found, try checking your internet connection.\n\nInstalled Version: {Properties.Settings.Default.Version}", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void SaveParsed_Click(object sender, EventArgs e)
@@ -206,6 +184,17 @@ namespace Parser
             }
         }
 
+        private void CopyParsedToClipboard_Click(object sender, EventArgs e)
+        {
+            if (Parsed.Text.Length != 0)
+                Clipboard.SetText(Parsed.Text.Replace("\n", Environment.NewLine));
+        }
+
+        private void CheckForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CheckForUpdates(true);
+        }
+
         private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show($"Chat Log Parser created by Maple for GTA World.\n\nInstalled Version: {Properties.Settings.Default.Version}\n\nWould you like to visit the repository page on GitHub?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
@@ -217,15 +206,15 @@ namespace Parser
             Application.Exit();
         }
 
-        private void CheckForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            CheckForUpdates(true);
-        }
-
         private void Logo_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show($"Would you like to open the documentation page for the chat log parser found on the GTA World forums?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
                 System.Diagnostics.Process.Start(Data.versionLocation);
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveSettings();
         }
     }
 }
