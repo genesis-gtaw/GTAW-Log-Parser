490781449b079f0c6361e182b77e76d8fc9ab661
David <david.cristian.horvath@gmail.com>
2018-09-24 18:27:21 +0300

    Fixed settings not saving that caused an error while trying to set up automatic backups on the first launch

--- Parser/Properties/Settings.Designer.cs
+++ Parser/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Parser.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.8")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.9")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
