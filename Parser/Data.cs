4a12e2d7e2efdbeb19eb3e22a445e0cf218431f6
David <david.cristian.horvath@gmail.com>
2018-11-10 10:47:20 +0200

    Sort backups in folders

--- Parser/Data.cs
+++ Parser/Data.cs
@@ -4,6 +4,7 @@ namespace Parser
 {
     class Data
     {
+        public static readonly string processName = "GTA5";
         public static readonly string productHeader = "GTAW-Log-Parser";
         public static readonly string serverIP = "164.132.206.209_22005";
         public static readonly string logLogation = $"client_resources\\{serverIP}\\.storage";
