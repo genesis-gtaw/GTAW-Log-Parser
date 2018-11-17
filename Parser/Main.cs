3aa928789c9e80fc901e5cc3f059151c0ca87b74
Maple <19516122+MapleToo@users.noreply.github.com>
2018-11-17 17:42:03 +0200

    Update parsing to new format

--- Parser/Main.cs
+++ Parser/Main.cs
@@ -158,14 +158,16 @@ namespace Parser
                     log = sr.ReadToEnd();
                 }
 
-                log = log.Remove(0, 1);                             // Remove the `"` character from the start
+                log = Regex.Match(log, "\\\"chatlog\\\":\\\".+\\\\n\\\"").Value;
+
+                log = log.Replace("\"chatlog\":\"", "");
                 log = log.Replace("\\n", "\n");                     // Change all occurrences of `\n` into new lines
-                log = log.Remove(log.Length - 2, 2);                // Remove the `new line` and the `"` character from the end
+                log = log.Remove(log.Length - 2, 2);                // Remove the `new line` and the `"}` characters from the end
 
                 //log = Regex.Replace(log, "~[A-Za-z]~", string.Empty);                   // Remove the RAGEMP color tags (example: `~r~` for red)
                 //log = Regex.Replace(log, @"!{#(?:[0-9A-Fa-f]{3}){1,2}}", string.Empty); // Remove HEX color tags (example: `!{#FFEC8B}` for the yellow color picked for radio messages)
 
-                log = Regex.Replace(log, "<[^>]*>", string.Empty);  // Remove the HTML tags that are added for the chat (example: `If the ingame menus are out of place, use <span style=\"color: dodgerblue\">/movemenu</span>`)
+                //log = Regex.Replace(log, "<[^>]*>", string.Empty);  // Remove the HTML tags that are added for the chat (example: `If the ingame menus are out of place, use <span style=\"color: dodgerblue\">/movemenu</span>`)
                 log = System.Net.WebUtility.HtmlDecode(log);        // Decode HTML symbols (example: `&apos;` into `'`)
                 previousLog = log;
 
--- Parser/Properties/Settings.Designer.cs
+++ Parser/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Parser.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.4")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
